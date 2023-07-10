33675168d938417513b1f29f4883ea91fe2a5238
寺澤徳隆 <mosqey1889@gmail.com>
2023-07-10 22:00:34 +0900

    topボタンの作成

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  // 100pxを境にTOPに戻るボタンの表示・非表示を切り替える  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#page-top').fadeIn();
+    } else {
+      $('#page-top').fadeOut();
+    }
+  });
+
+});
