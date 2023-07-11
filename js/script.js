c791631ad096b954450c6ed6cbe59582d7349f87
寺澤徳隆 <mosqey1889@gmail.com>
2023-07-11 21:54:55 +0900

    スムーズスクロール機能追加

--- js/script.js
+++ js/script.js
@@ -11,12 +11,31 @@ window.addEventListener('DOMContentLoaded', function () {
 
 });
 
+// Worksの画像をクリックしたときにモーダルで拡大表示する
 window.addEventListener('DOMContentLoaded', function(){
-  $("#bicycle img").click(function() {
+  $("#works img").click(function() {
     $("#graydisplay").html($(this).prop('outerHTML'));
     $("#graydisplay").fadeIn(200);
   });
   $("#graydisplay, #graydisplay img").click(function() {
     $("#graydisplay").fadeOut(200);
   });
+});
+
+// ページ内リンクのスクロールをなめらかにする（スムーズスクロール）
+window.addEventListener('DOMContentLoaded', function(){
+$('a[href^="#"]').click(function () {
+  const speed = 500;
+  const href = $(this).attr('href');
+  let $target;
+  if (href == '#') {
+    $target = $('html');
+  }
+  else {
+    $target = $(href);
+  }
+  const position = $target.offset().top;
+  $('html, body').animate({ 'scrollTop': position }, speed, 'swing');
+  return false;
+})
 });
