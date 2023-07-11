c14b24b408c12e86a324c7cd583a53a147f6769c
寺澤徳隆 <mosqey1889@gmail.com>
2023-07-11 21:22:27 +0900

    モーダルウインドウの実装

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 
 });
+
+window.addEventListener('DOMContentLoaded', function(){
+  $("#bicycle img").click(function() {
+    $("#graydisplay").html($(this).prop('outerHTML'));
+    $("#graydisplay").fadeIn(200);
+  });
+  $("#graydisplay, #graydisplay img").click(function() {
+    $("#graydisplay").fadeOut(200);
+  });
+});
