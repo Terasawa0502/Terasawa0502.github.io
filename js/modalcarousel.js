a86b591d7528f34f011d15d596df5ea1c4a9355a
寺澤徳隆 <mosqey1889@gmail.com>
2023-08-17 22:08:56 +0900

    モーダル表示用のjsファイルの仮用意

---
+++ js/modalcarousel.js
@@ -0,0 +1,17 @@
+function openImg1 () {
+  var image = document.getElementById('image1');
+  var source = image.src;
+  window.open(source);
+};
+
+function openImg2 () {
+  var image = document.getElementById('image2');
+  var source = image.src;
+  window.open(source);
+};
+
+function openImg3 () {
+  var image = document.getElementById('image3');
+  var source = image.src;
+  window.open(source);
+};
