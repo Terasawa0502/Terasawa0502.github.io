17be5e190ee6a8fc9e2b1f05ed878b0ea6f7cfc3
寺澤徳隆 <mosqey1889@gmail.com>
2023-08-23 22:48:17 +0900

    モーダル表示用の雛形作成

--- js/modalcarousel.js
+++ js/modalcarousel.js
@@ -1,17 +1,40 @@
-function openImg1 () {
-  var image = document.getElementById('image1');
-  var source = image.src;
-  window.open(source);
-};
+window.addEventListener('DOMContentLoaded', function(e) {
+//要素を取得
+const modal = document.querySelector('.js-modal'),
+image1 = document.getElementById('image1'),
+image2 = document.getElementById('image2'),
+image3 = document.getElementById('image3'),
+close = document.querySelector('.js-modal-close');
 
-function openImg2 () {
-  var image = document.getElementById('image2');
-  var source = image.src;
-  window.open(source);
-};
+image1.addEventListener('click', modalOpen1);
+image2.addEventListener('click', modalOpen2);
+image3.addEventListener('click', modalOpen3);
 
-function openImg3 () {
-  var image = document.getElementById('image3');
-  var source = image.src;
-  window.open(source);
-};
+//「開くボタン」をクリックしてモーダルを開く
+function modalOpen1() {
+  modal.classList.add('is-active');
+}
+
+function modalOpen2() {
+  modal.classList.add('is-active');
+}
+
+function modalOpen3() {
+  modal.classList.add('is-active');
+}
+
+//「閉じるボタン」をクリックしてモーダルを閉じる
+function modalClose() {
+  modal.classList.remove('is-active');
+}
+close.addEventListener('click', modalClose);
+
+//「モーダルの外側」をクリックしてモーダルを閉じる
+function modalOut(e) {
+  if (e.target == modal) {
+    modal.classList.remove('is-active');
+  }
+}
+addEventListener('click', modalOut);
+
+});
