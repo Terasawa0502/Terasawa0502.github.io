696329134376098084fcd8720d35fb81cc2e707c
寺澤徳隆 <mosqey1889@gmail.com>
2023-09-04 21:48:45 +0900

    カルーセル機能の準備

--- js/modalcarousel.js
+++ js/modalcarousel.js
@@ -4,7 +4,9 @@ const modal = document.querySelector('.js-modal'),
 image1 = document.getElementById('image1'), 
 image2 = document.getElementById('image2'),
 image3 = document.getElementById('image3'),
-appImg = this.document.getElementById('application-img'),
+slider = this.document.getElementById('slider'),
+slide = this.document.querySelectorAll('slide'),
+contents = this.document.querySelectorAll('.slide-content'),
 environment = this.document.getElementById('environment'),
 summary = this.document.getElementById('summary'),
 background = this.document.getElementById('background'),
@@ -13,6 +15,11 @@ point = this.document.getElementById('point'),
 assignment = this.document.getElementById('assignment'),
 close = document.querySelector('.js-modal-close');
 
+// コンソールアプリ写真の配列
+var consoleImg = ['<img src="img/console1.png" alt="テキスト">',
+                  '<img src="img/console2.png" alt="テキスト">',
+                  '<img src="img/console3.png" alt="テキスト">'];
+
 image1.addEventListener('click', modalOpen1);
 image2.addEventListener('click', modalOpen2);
 image3.addEventListener('click', modalOpen3);
@@ -20,11 +27,20 @@ image3.addEventListener('click', modalOpen3);
 //「開くボタン」をクリックしてモーダルを開く
 function modalOpen1() {
   modal.classList.add('is-active');
-  appImg.innerHTML = '<img src="img/console1.png" alt="テキスト">';
+
+// slide-contentを順に取得して写真を挿入していく
+  for (let i in contents) {
+    if (contents.hasOwnProperty(i)) {
+      contents[i].innerHTML = consoleImg[i];
+    }
+  }
+
+  // content.innerHTML = '<img src="img/console1.png" alt="テキスト">';
   environment.textContent = 'Java/Intelli J IDEA';
   summary.textContent = '';
   background.textContent = '';
-  github.textContent = 'https://github.com/Terasawa0502/BlackJack';
+  // ToDo: 後でリンクにする
+  github.textContent = 'https://github.com/Terasawa0502/BlackJack'; 
   point.textContent = '';
   assignment.textContent = '';
 }
